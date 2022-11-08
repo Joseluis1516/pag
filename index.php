8bd194f506d49c3ef370b1fa23983acff24a7811
jose luis <joseluislopeztorres14@gmail.com>
2022-11-08 12:37:38 -0700

    index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <img src="logo.png" alt="">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="pagina2.php">Menu</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <h1>Cenaduria Doña Rita</h1>
+    <pre>Son tostadas y gorditas comida casera hecha en hornillas muy ricas,
+baratas y con muy buena atención se les atiende.</pre>
+
+    <p>Contactos <br>
+        Facebook: cenaduría doña Rita <br>
+        Cel: 69411855996 o 6941165283</p>
+</body>
+
+</html>
