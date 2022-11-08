6c2eacac06a9e326f11ddaa8b4c87843b5ec6864
jose luis <joseluislopeztorres14@gmail.com>
2022-11-08 12:38:06 -0700

    pagina2.php

---
+++ pagina2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="etilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Tostadas y gorditas <br><br>
+        De Carne, Pollo, Higado, Bofe, Corazon, Manita de Puerco(entomatada o escabeches), Pieza.
+    </p>
+
+    <a href="index.php">Regresar</a>
+</body>
+</html>
