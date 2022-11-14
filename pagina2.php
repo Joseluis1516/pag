f51706b1648397bb8f23f679416b0ff975fa6ae8
jose luis <joseluislopeztorres14@gmail.com>
2022-11-14 12:23:40 -0700

    pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -1,18 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<?php include "cabecera.php" ?>
+
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="etilos.css">
+    <!-- css only -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="estilos.css">
+    <!-- javaScript  -->
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <title>Document</title>
 </head>
-<body>
-    <p>
-        Tostadas y gorditas <br><br>
-        De Carne, Pollo, Higado, Bofe, Corazon, Manita de Puerco(entomatada o escabeches), Pieza.
-    </p>
 
-    <a href="index.php">Regresar</a>
+<body style="background-color: #f4b184; ">
+    <div class="cuatro">
+        <pre>
+        <b> <h5> Tostadas y Gorditas</h5></b> <br>
+            De Carne, Pollo, Higado, Bofe, Corazon,
+            Manita de Puerco(entomatada o escabeches), Pieza,
+            orden asado.
+        </pre>
+    </div>
+    <div class="cinco">
+        <pre>
+            <b>Bebidas</b>
+            <p>Coca cola, Fanta, Agua de cebada,
+Agua de nanchi, Agua de piña</p>
+        </pre>
+    </div>
+    <table class="tabla">
+        <td>
+            <img src="img/to.png" class="img">
+            <img src="img/asa.png" class="img">
+            <img src="img/tos.png" class="img">
+            <img src="img/lo.png" class="img">
+            <img src="img/go.png" class="img">
+            <img src="img/me.png" class="img">
+        </td>
+
+    </table>
+    <br>
+    <!-- <button class="btn btn-light"> <a  href="index.php">Regresar</a> </button> -->
+
 </body>
 </html>
+
+<div style="background-color: #DCDCDC;">
+    <?php include('pie.php') ?>
+</div>
