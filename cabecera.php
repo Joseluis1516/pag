4163013e8d1a11ed6f35a57fa4223334dd81266f
jose luis <joseluislopeztorres14@gmail.com>
2022-11-14 12:22:00 -0700

    cabecera.php

---
+++ cabecera.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css only -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="estilos.css">
+    <!-- javaScript  -->
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+   
+    <nav class="navbar bg-light">
+  <div>
+  <a href="index.php"><img src="img/logo.png" alt="" height="50" width="50"></a>
+  <a class="navbar-brand" href="pagina2.php" >Menu</a>
+  </div>
+</nav>
+</body>
+
+</html>
